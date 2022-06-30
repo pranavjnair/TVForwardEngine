8de811478419d10fcb4ed5db0c31c7c4f385172a
Pranav Nair <pranavnair@Pranavs-MacBook-Pro.local>
2022-06-29 22:00:44 -0500

    working on connecting to postgress server

---
+++ com.purple.engine/dataloader/DataLoader.py
@@ -0,0 +1,30 @@
+import psycopg2
+import psycopg2.extras as psql_extras
+import pandas as pd
+
+def insert_data(
+        query: str,
+        conn: psycopg2.extensions.connection,
+        cur: psycopg2.extensions.cursor,
+        df: pd.DataFrame,
+        page_size: int
+) -> None:
+    data_tuples = [tuple(row.to_numpy()) for index, row in df.iterrows()]
+
+    try:
+        psql_extras.execute_values(
+            cur, query, data_tuples, page_size=page_size)
+        print("Query:", cur.query)
+
+    except Exception as error:
+        print(f"{type(error).__name__}: {error}")
+        print("Query:", cur.query)
+        conn.rollback()
+        cur.close()
+
+    else:
+        conn.commit()
+
+if __name__ == "__main__":
+    connection = conn = psycopg2.connect(dbname="local_tvforward", user="postgres", password="secret")
+
--- com.purple.engine/scrape/VarietyScraper.py
+++ com.purple.engine/scrape/VarietyScraper.py
@@ -4,48 +4,48 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
-session = requests.Session()
 
-session.headers.update(
-    {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
-    }
-)
+def run_variety_scraper():
+    session = requests.Session()
 
-page_number = 1
-base_url = 'https://variety.com/v/tv/'
-page_url = 'https://variety.com/v/tv/page/' + str(page_number) + '/'
-page_limit = 10
+    session.headers.update(
+        {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
+        }
+    )
 
-try:
-    news_list = []
-    for i in range(page_number, page_limit + 1):
-        if i == 1:
-            session_url = base_url
-        else:
-            session_url = base_url + 'page/' + str(i) + '/'
-        web_page = session.get(session_url)
-        soup = BeautifulSoup(web_page.text, "html.parser")
-        web_page_attrs = soup.find_all('li', attrs={'class': 'o-tease-list__item'})
+    page_number = 1
+    base_url = 'https://variety.com/v/tv/'
+    page_limit = 10
 
-        news_sub_list = []
-        for attr in web_page_attrs:
-            title = attr.find("h3", attrs={'id': 'title-of-a-story'})
-            if title is None:
-                break
+    try:
+        news_list = []
+        for i in range(page_number, page_limit + 1):
+            if i == 1:
+                session_url = base_url
             else:
-                title = title.find('a').text.strip()
-            link = attr.find("h3", attrs={'id': 'title-of-a-story'})
-            if link is None:
-                break
-            else:
-                link = link.find('a')['href']
-            date = datetime.date.today().strftime("%Y-%m-%d")
+                session_url = base_url + 'page/' + str(i) + '/'
+            web_page = session.get(session_url)
+            soup = BeautifulSoup(web_page.text, "html.parser")
+            web_page_attrs = soup.find_all('li', attrs={'class': 'o-tease-list__item'})
 
-            news_list.append([title, link, date])
+            for attr in web_page_attrs:
+                title = attr.find("h3", attrs={'id': 'title-of-a-story'})
+                if title is None:
+                    break
+                else:
+                    title = title.find('a').text.strip()
+                link = attr.find("h3", attrs={'id': 'title-of-a-story'})
+                if link is None:
+                    break
+                else:
+                    link = link.find('a')['href']
+                date = datetime.date.today().strftime("%Y-%m-%d")
 
-    variety_data = pd.DataFrame(news_list, columns=['Title', 'Link', 'Date'])
-    # print(variety_data)
+                news_list.append([title, link, date])
 
-except Exception as e:
-    print(e.__str__())
+        variety_data = pd.DataFrame(news_list, columns=['Title', 'Link', 'Date'])
+        return variety_data
+
+    except Exception as e:
+        print(e.__str__())
